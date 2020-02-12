9623b0fb0035224397b1e38bdc1cf1df6617eb6e
Claudio Bosetti <57715060+claudiobosetti@users.noreply.github.com>
2020-02-12 19:03:46 +0100

    minor changes

--- README.md
+++ README.md
@@ -1,19 +1,18 @@
 <h1 align="center" style="font-size: 3rem;">
-Tiktokbot
+TikTokBot :snake: :fire:
 </h1>
 <p align="center">
- <em>Tiktokbot auto macro with image recognition.</em></p>
+ <em>TikTokBot auto macro with image recognition.</em></p>
 
-<p align="center"><h2>Install process</h2><p>
-Install bluestacks<p>
-Go into tiktok app with bluestacks home screen<p>
-python run.py
+<p align="center"><h2>Installation :gear:</h2><p>
 
-You need to have basic knowledge of python and tiktok for the bot to work.. Don't except to open bluestacks and the script magicaly works.. Most likely you need to take new pictures of images if you don't use same screen-size.
+- Install Bluestacks
 
-I will not make this bot work on all platforms without users need to modify it.. it would take me days-weeks.. and i don't get paid for it.. so not worth my time. 
- <p>
-  
-  ### Demo:
+- Open TikTok and select Home
+
+- Open terminal and type py run.py or python3 run.py
  
- ![](img/demo.gif)
+  
+### Demo :robot:
+ 
+![](img/demo.gif)
--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import pyautogui
 import time
 import random
-screenWidth, screenHeight = pyautogui.size() # Returns two integers, the width and height of the screen. (The primary monitor, in multi-monitor setups.
+screenWidth, screenHeight = pyautogui.size() # width and height of the screen
 
 
-currentMouseX, currentMouseY = pyautogui.position() # Retursn two integes, the x and y of the mouse cursor's current position.
+currentMouseX, currentMouseY = pyautogui.position() # x and y of the mouse cursor's current position.
 class Bot():
     
     def __init__ (self, x=0):
@@ -12,11 +12,11 @@ class Bot():
         self.home = pyautogui.locateOnScreen('img/home.png', confidence=0.9)
 
     def get_around_ads(self, sleep):
-            pyautogui.click(35, 140) # close left side
+            pyautogui.click(35, 140) 
             time.sleep(sleep)
-            pyautogui.click(35, 140) # close left side
+            pyautogui.click(35, 140) 
             time.sleep(0.7)
-            pyautogui.click(35, 140) # close left side
+            pyautogui.click(35, 140) 
             time.sleep(0.7)
 
     def liker(self, sleep):
@@ -28,7 +28,7 @@ class Bot():
         print(str(self.x))
 
     def comment_liker(self, sleep):
-            pyautogui.click(95, 10) # go back to tiktok app
+            pyautogui.click(95, 10) # back to tiktok app
             time.sleep(0.6)
             pyautogui.click(420, 450)
             time.sleep(0.6)
@@ -45,15 +45,14 @@ class Bot():
                 i += 1
                 if like == None:
                     break
-            #like = pyautogui.locateOnScreen('like.png', confidence=0.9) 
+            
             pyautogui.click(like)
-            #pyautogui.click(410, 190)
             time.sleep(0.3)
             pyautogui.click(420, 120)
             time.sleep(0.4)
             self.get_around_ads(1)
             time.sleep(sleep)
-            pyautogui.click(95, 10) # go back to tiktok app
+            pyautogui.click(95, 10) 
             pyautogui.click(40, 790)
             time.sleep(0.3)
 
@@ -62,10 +61,10 @@ class Bot():
         pyautogui.click(confirm)
 
     def comment(self, sleep):
-            comment = ["this is cool", "yeaah", "hehe", "hehehe", ";)", ":)", "wow", "wooow", "nice", "cool", "awesome", "yes", "this is awesome", "agree"]
+            comment = ["cool","great","fire","awesome","nice","noice","lol","lmao"]
             time.sleep(2)
             comments = pyautogui.locateOnScreen('img/comment.png', confidence=0.8)
-            print("does comment box exist", comments)
+            print("Comment box esists", comments)
             time.sleep(1)
             pyautogui.click(comments)
             time.sleep(1)
@@ -85,7 +84,7 @@ class Bot():
             time.sleep(2)
             pyautogui.click(close)
             time.sleep(1)
-   #         self.confirm()
+   
             time.sleep(sleep)
             self.x += 1
             print(str(self.x))
@@ -97,9 +96,9 @@ class Bot():
 
     def follow_user(self, sleep):
         time.sleep(1)
-        follow_buttom = pyautogui.locateOnScreen('img/follow.png', confidence=0.8)
-        print("does follow buttom exist", follow_buttom)
-        pyautogui.click(follow_buttom)
+        follow_button = pyautogui.locateOnScreen('img/follow.png', confidence=0.8)
+        print("Follow button exists", follow_button)
+        pyautogui.click(follow_button)
         time.sleep(1)
         self.go_back_page()
         time.sleep(sleep)
@@ -112,10 +111,9 @@ while True:
     time.sleep(2)
     pyautogui.click(home)
     time.sleep(4)
-    #bot.comment_liker(5)
+    
     bot.follow_user(65 + random.random())
     time.sleep(10)
-    #bot.comment(10)
+    
     bot.comment(4)
     time.sleep(3)
-   # bot.liker(1 + random.random())
